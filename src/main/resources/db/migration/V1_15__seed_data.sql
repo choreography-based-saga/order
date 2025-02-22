68be8e76120959e038a9eb46ae9c99805529cb80
marijal74 <lakic.marija98@gmail.com>
2025-02-22 17:59:18 +0100

    Seed data

---
+++ src/main/resources/db/migration/V1_15__seed_data.sql
@@ -0,0 +1,9 @@
+insert into merchant_address
+values (1, 'RETURN', 'Pozarevac', 12000, 'Nemanjina', 4),
+       (2, 'PICKUP', 'Pozarevac', 12000, 'Nemanjina', 10);
+
+insert into merchant
+values (1, '123456789', 'Trgovac d.o.o', 2, 1, 'cd7b8afa-603b-4dee-9c65-5b32e01c4d2f');
+
+insert into merchant_product
+values (1, 'TV', 'TV najnovije generacije', true, 2, 1, 10, 5, 1, 5000.00)
